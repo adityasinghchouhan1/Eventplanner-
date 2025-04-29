e13825bcedee5b0d92f76b637d5dfd75519100fc
aditya <asadityasingh2572000@gmail.com>
2025-04-29 11:46:59 +0530

    create other compoents like Home,about etc

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <>
       <div>
+        <Navbar />
         <Outlet />
+        <Footer />
       </div>
     </>
   )
--- Client/src/Router/Router.jsx
+++ Client/src/Router/Router.jsx
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
+import Home from '../Components/Home'
+import About from '../Components/About'
+import Contect from '../Components/Contect'
+import Services from '../Components/Services'
 
 const router = createBrowserRouter([
   {
     path: '',
     element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'About',
+        element: <About />,
+      },
+      {
+        path: 'Contect',
+        element: <Contect />,
+      },
+      {
+        path: 'Services',
+        element: <Services />,
+      },
+    ],
   },
 ])
 
