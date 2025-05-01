5d5c99d6da3f764f22c9b7b7825f59e6d7cdce7c
aditya <asadityasingh2572000@gmail.com>
2025-05-01 12:16:01 +0530

    complete heading stayling

--- Client/src/Resue/Heading.jsx
+++ Client/src/Resue/Heading.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Heading = ({ title, discription }) => {
   return (
     <>
-      <div className="flex justify-center items-center flex-col text-center">
-        <h1 className="text-center text-4xl">{title}</h1>
-        <p className="text-center text-xs mt-7 sm:px-60 px-5 ">{discription}</p>
+      <div className="flex justify-center items-center flex-col text-center my-7">
+        <h1 className="text-center text-6xl font-Yellowtail2">{title}</h1>
+        <p className="text-center text-xs sm:text-sm mt-7 sm:px-60 px-5 font-Yellowtail text-gray-500">
+          {discription}
+        </p>
       </div>
     </>
   )
