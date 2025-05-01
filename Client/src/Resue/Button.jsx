ff37d30f8150b990917b36fbb446ee839c6d4a93
aditya <asadityasingh2572000@gmail.com>
2025-05-01 12:15:27 +0530

    complete button styling

--- Client/src/Resue/Button.jsx
+++ Client/src/Resue/Button.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 
 const Button = ({ title, OnClick }) => {
   return (
-    <>
-      <button
-        className="border border-black py-2 px-4 hover:border-yellow-200 transition-all"
-        onClick={OnClick}
-      >
-        {title}
-      </button>
-    </>
+    <button className="custom-button" onClick={OnClick}>
+      {title}
+    </button>
   )
 }
 
