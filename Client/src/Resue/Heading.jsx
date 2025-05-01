f4139cb6f1a9fb6ea7e061704fb6f007934b170b
aditya <asadityasingh2572000@gmail.com>
2025-05-01 16:06:37 +0530

    add animation

--- Client/src/Resue/Heading.jsx
+++ Client/src/Resue/Heading.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
+gsap.registerPlugin(ScrollTrigger)
 const Heading = ({ title, discription }) => {
+  const headingRef = useRef(null)
+  useEffect(() => {
+    const element = headingRef.current
+    if (!element) return
+
+    const ctx = gsap.context(() => {
+      gsap.from(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: 'top 70%',
+          toggleActions: 'play reverse play reverse',
+          markers: false,
+        },
+        x: -100, // animate from left to right
+        opacity: 0,
+        duration: 1,
+        ease: 'power3.out',
+      })
+    }, headingRef)
+    return () => ctx.revert()
+  }, [])
   return (
     <>
-      <div className="flex justify-center items-center flex-col text-center my-7">
+      <div
+        className="flex justify-center items-center flex-col text-center my-7 "
+        ref={headingRef}
+      >
         <h1 className="text-center text-6xl font-Yellowtail2">{title}</h1>
         <p className="text-center text-xs sm:text-sm mt-7 sm:px-60 px-5 font-Yellowtail text-gray-500">
           {discription}
