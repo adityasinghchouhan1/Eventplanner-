c8f92a763f6e7c0fa8deecc6b39eba9ac7ba098e
aditya <asadityasingh2572000@gmail.com>
2025-05-10 13:03:45 +0530

    complete protfolio component styling

--- Client/src/Components/Portfolio.jsx
+++ Client/src/Components/Portfolio.jsx
@@ -8,7 +8,7 @@ import p4 from '../../public/p4.jpg'
 const Portfolio = () => {
   return (
     <>
-      <div className="flex justify-center items-center sm:flex-col p-5">
+      <div className="flex justify-center items-center flex-col  p-5">
         <Heading
           title={'Our Portfolio'}
           discription={
@@ -16,60 +16,68 @@ const Portfolio = () => {
           }
         />
         <div className="relative flex flex-col justify-center items-center">
-          <div className="flex ">
-            <div className=" overflow-hidden hover:scale-90 duration-500 transition-all">
+          <div className="flex sm:flex-row flex-col justify-around gap-5 items-center">
+            <div className=" group w-full md:w-1/2  overflow-hidden hover:scale-90 duration-500 transition-all">
               <img
                 src={p4}
-                className="bg-center bg-cover hover:scale-125 duration-500 transition-all"
+                className="w-[400px] z-0 object-cover bg-center bg-cover group-hover:scale-125 duration-500 transition-all"
               />
-              <h1 className=" font-inter text-2xl text-gray-600 uppercase mt-2">
-                Our Portfolio
-              </h1>
-              <p2 className=" font-Yellowtail2 text-xs text-gray-500">
-                ensuring a flawless and memorable experience for our clients and
-                their guests.
-              </p2>
+              <div className=" text-gray-500 group-hover:text-white group-hover:bg-black/25 duration-500 transition-all p-1">
+                <h1 className="z-20 relative font-inter text-2xl uppercase mt-2 group-hover:font-semibold duration-500 transition-all">
+                  Our Portfolio
+                </h1>
+                <p className="z-20 relative font-Yellowtail2 text-xs ">
+                  ensuring a flawless and memorable experience for our clients
+                  and their guests.
+                </p>
+              </div>{' '}
             </div>
-            <div className=" overflow-hidden hover:scale-90 duration-500 transition-all">
+            <div className="group w-full md:w-1/2 overflow-hidden hover:scale-90 duration-500 transition-all">
               <img
                 src={p}
-                className="bg-center bg-cover hover:scale-125 duration-500 transition-all"
+                className="w-[500px] z-0 object-cover bg-center bg-cover group-hover:scale-125 duration-500 transition-all"
               />
-              <h1 className=" font-inter text-2xl text-gray-600 uppercase mt-2">
-                Our Portfolio
-              </h1>
-              <p2 className=" font-Yellowtail2 text-xs text-gray-500">
-                ensuring a flawless and memorable experience for our clients and
-                their guests.
-              </p2>
+              <div className=" text-gray-500 group-hover:text-white group-hover:bg-black/25 duration-500 transition-all p-1">
+                <h1 className="z-20 relative font-inter text-2xl uppercase mt-2 group-hover:font-semibold duration-500 transition-all">
+                  Our Portfolio
+                </h1>
+                <p className="z-20 relative font-Yellowtail2 text-xs">
+                  ensuring a flawless and memorable experience for our clients
+                  and their guests.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="flex">
-            <div className=" overflow-hidden hover:scale-90 duration-500 transition-all">
-              <img
-                src={p2}
-                className="bg-center bg-cover hover:scale-125 duration-500 transition-all"
-              />
-              <h1 className=" font-inter text-2xl text-gray-600 uppercase mt-2">
-                Our Portfolio
-              </h1>
-              <p2 className=" font-Yellowtail2 text-xs text-gray-500">
-                ensuring a flawless and memorable experience for our clients and
-                their guests.
-              </p2>
-            </div>
-            <div className=" overflow-hidden hover:scale-90 duration-500 transition-all">
+          <div className="flex sm:flex-row flex-col justify-around gap-10 items-center">
+            <div className="group z-0 w-full md:w-1/2 overflow-hidden hover:scale-90 duration-500 transition-all">
               <img
                 src={p3}
-                className="bg-center bg-cover hover:scale-125 duration-500 transition-all"
+                className="w-[400px] object-cover bg-center bg-cover group-hover:scale-125 duration-500 transition-all"
               />
-              <h1 className=" font-inter text-2xl text-gray-600 uppercase mt-2">
-                Our Portfolio
-              </h1>
-              <p2 className=" font-Yellowtail2 text-xs text-gray-500">
-                ensuring a flawless and memorable experience for our clients and
-                their guests.
-              </p2>
+              <div className=" text-gray-500 group-hover:text-white group-hover:bg-black/25 duration-500 transition-all p-1">
+                <h1 className="z-20 relative font-inter text-2xl uppercase mt-2 group-hover:font-semibold duration-500 transition-all">
+                  Our Portfolio
+                </h1>
+                <p className="z-20 relative font-Yellowtail2 text-xs">
+                  ensuring a flawless and memorable experience for our clients
+                  and their guests.
+                </p>
+              </div>
+            </div>
+            <div className="group w-full md:w-1/2 overflow-hidden hover:scale-90 duration-500 transition-all p-2">
+              <img
+                src={p2}
+                className="z-0 w-[400px] sm:w-[300px] object-cover bg-center bg-cover group-hover:scale-125 duration-500 transition-all"
+              />
+              <div className=" text-gray-500 group-hover:text-white group-hover:bg-black/25 duration-500 transition-all p-1">
+                <h1 className="z-20 relative font-inter text-2xl uppercase mt-2 group-hover:font-semibold duration-500 transition-all">
+                  Our Portfolio
+                </h1>
+                <p className="z-20 relative font-Yellowtail2 text-xs">
+                  ensuring a flawless and memorable experience for our clients
+                  and their guests.
+                </p>
+              </div>
             </div>
           </div>
         </div>
