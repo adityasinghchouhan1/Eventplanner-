aa0bd5f23b882f755283ea6b06856f6b84b2a357
aditya <asadityasingh2572000@gmail.com>
2025-05-10 14:06:09 +0530

    complete wrapper styling

--- Client/src/Components/About.jsx
+++ Client/src/Components/About.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import Wrapper from '../Resue/Wrapper'
+import aboutW from '../../public/wapper.jpg'
 const About = () => {
-  return <div>About</div>
+  return (
+    <>
+      <div>
+        <Wrapper wimg={aboutW} title={'About us'} />
+      </div>
+    </>
+  )
 }
 
 export default About
--- Client/src/Resue/Wrapper.jsx
+++ Client/src/Resue/Wrapper.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
-const Wrapper = () => {
-  return <div>Wrapper</div>
+const Wrapper = ({ wimg, title }) => {
+  return (
+    <>
+      <div className="px-10 py-2 ">
+        <div
+          style={{ backgroundImage: `url(${wimg})` }}
+          className="bg-center object-fit-contain bg-no-repeat flex justify-center items-center h-[80vh]"
+        >
+          <div
+            className="text-6xl font-Yellowtail2 text-white uppercase tracking-widest
+ [word-spacing:20px]"
+          >
+            {title}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Wrapper
