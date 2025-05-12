09d1891cfe8c3697ed617f5bd168869b382bf702
aditya <asadityasingh2572000@gmail.com>
2025-05-12 16:10:41 +0530

    img

--- Client/src/Resue/Wrapper.jsx
+++ Client/src/Resue/Wrapper.jsx
@@ -6,10 +6,10 @@ const Wrapper = ({ wimg, title }) => {
       <div className="px-10 py-2 ">
         <div
           style={{ backgroundImage: `url(${wimg})` }}
-          className="bg-center object-fit-contain bg-no-repeat flex justify-center items-center h-[80vh]"
+          className="bg-center object-fit-contain bg-cover bg-no-repeat flex justify-center items-center h-[80vh] w-full"
         >
           <div
-            className="text-6xl font-Yellowtail2 text-white uppercase tracking-widest
+            className="text-6xl text-center font-Yellowtail2 text-white uppercase tracking-widest
  [word-spacing:20px]"
           >
             {title}
