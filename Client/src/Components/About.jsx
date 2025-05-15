33578e49e06f11ff5e782145c41a3b65b27cc8de
aditya <asadityasingh2572000@gmail.com>
2025-05-15 22:15:33 +0530

    add Aboutintro component

--- Client/src/Components/About.jsx
+++ Client/src/Components/About.jsx
@@ -3,11 +3,13 @@ import Wrapper from '../Resue/Wrapper'
 import aboutW from '../../public/wapper2.jpg'
 import HowWeAre from './HowWeAre'
 import Feedback from './Feedback'
+import AboutIntro from './AboutIntro'
 const About = () => {
   return (
     <>
       <div>
         <Wrapper wimg={aboutW} title={'About us'} />
+        <AboutIntro />
         <HowWeAre />
         <Feedback />
       </div>
