24a3d748c6e261f0b339dbfd3c366be0d5de5f90
aditya <asadityasingh2572000@gmail.com>
2025-05-15 21:05:07 +0530

    update sidebar layout

--- Admin/src/Components/Sidebar.jsx
+++ Admin/src/Components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FaBars } from 'react-icons/fa'
-import { Outlet, Link, Links } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false)
   const ToggleOpenClose = () => {
@@ -8,34 +8,41 @@ const Sidebar = () => {
   }
   return (
     <>
-      <div className="flex bg-gray-400 h-screen">
+      <div className="flex bg-gray-100 h-screen">
         <aside
           className={`${
-            isOpen ? 'w-16' : 'w-64'
-          } fixed transition-all duration-300 h-gull bg-blue-950 border-r-4 border-yellow-900`}
+            isOpen ? 'w-52' : 'w-16'
+          } fixed transition-all duration-300 h-full bg-blue-950 border-r-4 border-yellow-900`}
         >
           <div className="">
-            {!isOpen && (
+            {isOpen && (
               <div className="text-3xl font-semibold w-full text-center">
                 ADMIN
               </div>
             )}
-            <button onClick={ToggleOpenClose}>
+            <button
+              onClick={ToggleOpenClose}
+              className="text-white p-2 ml-4 mt-4"
+            >
               <FaBars size={20} />
             </button>
           </div>
-          <nav className="mt-8">
-            <Links to="contect"></Links>
+          <nav className="mt-8 flex flex-col items-start pl-4 text-white">
+            {isOpen && (
+              <Link to="contect" className="mb-4 hover:underline">
+                Contact
+              </Link>
+            )}
           </nav>
         </aside>
         <div
           className={`${
             isOpen ? 'ml-60' : 'ml-16'
-          } transition-all duration-300  h-full`}
+          } transition-all duration-300  h-[100vh]`}
         >
-          <mian className="mt-6">
+          <main className="mt-6">
             <Outlet />
-          </mian>
+          </main>
         </div>
       </div>
     </>
